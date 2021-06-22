347b552590a58501ed45d1f47755ecd824ad9078
Egor Mikhalevich <mevkysno@gmail.com>
2021-06-23 02:45:33 +0300

    Connecting Outlets and Actions
    Model-View-Controller
    Connecting Outlets and Actions
    
    Section 1
    Create Outlets in Interface Builder

---
+++ Today/Today/ReminderListCell.swift
@@ -0,0 +1,15 @@
+//
+//  ReminderListCell.swift
+//  Today
+//
+//  Created by Egor Mihalevich on 23.06.21.
+//
+
+import UIKit
+
+class ReminderListCell: UITableViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var doneButton: UIButton!
+    
+}
