67d77a6bb0d2b292f23bf6ab500a9efad5a8b3a9
Egor Mikhalevich <mevkysno@gmail.com>
2021-06-23 03:55:14 +0300

    Setting Up a Table View
    Section 2
    Add Data Source Methods

--- Today/Today/ReminderListCell.swift
+++ Today/Today/ReminderListCell.swift
@@ -9,14 +9,14 @@ import UIKit
 
 class ReminderListCell: UITableViewCell {
     typealias DoneButtonAction = () -> Void
-    
+
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var doneButton: UIButton!
-    
+
     var doneButtonAction: DoneButtonAction?
-    
+
     @IBAction func doneButtonTriggered(_ sender: UIButton) {
-            doneButtonAction?()
-        }
+        doneButtonAction?()
+    }
 }
