e931951f4cc85ded8579aa01611b4fe61aa95abb
Egor Mikhalevich <mevkysno@gmail.com>
2021-06-23 03:08:36 +0300

    Setting Up a Table View
    Section 1
    Add the Table View

---
+++ Today/Today/ReminderListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Egor Mihalevich on 22.06.21.
+//
+
+import UIKit
+
+class ReminderListViewController: UITableViewController {
+
+    
+
+
+}
+
